6ca2e0dd467fa05e4d00559dde07ff175dc32aa3
Darren Morman <darrenmorman@gmail.com>
2019-08-16 12:13:03 +0100

    Update endpoints after FPL update

--- src/FplLib.php
+++ src/FplLib.php
@@ -30,12 +30,12 @@ class FplLib
                 'operations' => [
                     'bootstrapStatic' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/bootstrap-static',
+                        'uri' => '/api/bootstrap-static/',
                         'responseModel' => 'getResponse',
                     ],
                     'entry' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/entry/{entryId}',
+                        'uri' => '/api/entry/{entryId}/',
                         'responseModel' => 'getResponse',
                         'parameters' => [
                             'entryId' => [
@@ -46,7 +46,7 @@ class FplLib
                     ],
                     'entryHistory' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/entry/{entryId}/history',
+                        'uri' => '/api/entry/{entryId}/history/',
                         'responseModel' => 'getResponse',
                         'parameters' => [
                             'entryId' => [
@@ -57,7 +57,7 @@ class FplLib
                     ],
                     'entryPicks' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/entry/{entryId}/event/{eventId}/picks',
+                        'uri' => '/api/entry/{entryId}/event/{eventId}/picks/',
                         'responseModel' => 'getResponse',
                         'parameters' => [
                             'entryId' => [
@@ -72,7 +72,7 @@ class FplLib
                     ],
                     'elementSummary' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/element-summary/{elementId}',
+                        'uri' => '/api/element-summary/{elementId}/',
                         'responseModel' => 'getResponse',
                         'parameters' => [
                             'elementId' => [
@@ -83,7 +83,7 @@ class FplLib
                     ],
                     'leaguesClassicStandings' => [
                         'httpMethod' => 'GET',
-                        'uri' => '/api/leagues-classic-standings/{leagueId}',
+                        'uri' => '/api/leagues-classic-standings/{leagueId}/',
                         'responseModel' => 'getResponse',
                         'parameters' => [
                             'leagueId' => [
